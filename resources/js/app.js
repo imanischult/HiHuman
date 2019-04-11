a50c33999f04a47e20903fb8cafab3b2280a2a4e
Rabbit <Corbineau@users.noreply.github.com>
2019-04-10 23:29:10 -0400

    Added firebase init

---
+++ resources/js/app.js
@@ -0,0 +1,11 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDY0ap1bIDwwvSRsvusGOtH1ow7F6AqPyU",
+    authDomain: "hiawesomehumans.firebaseapp.com",
+    databaseURL: "https://hiawesomehumans.firebaseio.com",
+    projectId: "hiawesomehumans",
+    storageBucket: "hiawesomehumans.appspot.com",
+    messagingSenderId: "501987551176"
+  };
+  firebase.initializeApp(config);
