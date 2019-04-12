80bf704b7f25c934e863bf74a500c4387483570d
Imani Schulterbrandt <imani.vicente@outlook.com>
2019-04-12 17:57:28 -0400

    initial modal html/css

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,3 +11,32 @@
   firebase.initializeApp(config);
 
   const db = firebase.database();
+
+
+  // ++++ Sign-Up Modal Logic ++++ //
+
+  // Get the modal
+var $modal = $('#signUpModal');
+
+// Get the button that opens the modal
+var $newAcctBtn = $("#newAccount");
+
+// Get the <span> element that closes the modal
+var $close = $(".close")[0];
+
+// When the user clicks the button, open the modal 
+$newAcctBtn.on('click', function() {
+  $modal.css('display','block')
+});
+
+// When the user clicks on <span> (x), close the modal
+$close.on('click', function() {
+  $modal.css('display','none')
+});
+
+// When the user clicks anywhere outside of the modal, close it
+window.on('click', function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+});
