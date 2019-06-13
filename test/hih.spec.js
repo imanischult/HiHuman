bd16f295b4fdc1b513e584fb192e16d81a0297d6
Imani Schulterbrandt <imani.vicente@outlook.com>
2019-06-13 15:33:44 -0400

    Adding testing files

---
+++ test/hih.spec.js
@@ -0,0 +1,10 @@
+// describe (title, callback) creates a Suite
+
+//test hih.spec.js
+
+//unit testing normally tests functions
+
+//assert module
+
+const assert = require('assert');
+const requestTime = require('../')
