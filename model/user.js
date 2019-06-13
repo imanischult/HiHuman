7f13d9df7511e6b29161237c2f54c4ffe01ca6a9
nreddix <47063889+nreddix@users.noreply.github.com>
2019-06-13 19:05:31 -0400

    Update model/user.js
    Co-Authored-By: Rabbit Stoddard <Corbineau@users.noreply.github.com>

--- model/user.js
+++ model/user.js
@@ -22,7 +22,7 @@ module.exports = function (sequelize, DataTypes) {
     // User.associate = function (models) {
     //     // We're saying that a Post should belong to an Author
     //     // A Post can't be created without an Author due to the foreign key constraint
-    //     Post.belongsTo(models.Author, {
+    //     User.hasMany(models.Relationship, {
     //         foreignKey: {
     //             allowNull: false
     //         }
