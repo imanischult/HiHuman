8747c7a154d5b35e6b8a48e23ae78c0f2c467cf5
Rabbit <Corbineau@users.noreply.github.com>
2019-06-15 13:51:22 -0400

    removing unnecessary files

--- models/user.js
+++ models/user.js
@@ -25,15 +25,15 @@ module.exports = function (sequelize, DataTypes) {
 
     });
 
-    // User.associate = function (models) {
-    //     // This will actually be User to Relationships``
-    //     // A Post can't be created without an Author due to the foreign key constraint
-    //     User.hasMany(models.Relationship, {
-    //         foreignKey: {
-    //             allowNull: false
-    //         }
-    //     });
-    // };
+    User.associate = function (models) {
+        // This will actually be User to Relationships``
+        // A Post can't be created without an Author due to the foreign key constraint
+        User.hasMany(models.Relationship, {
+            // foreignKey: {
+            //     allowNull: false
+            // }
+        });
+    };
 
     return User;
 };
