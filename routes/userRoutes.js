b99a72b70726f88dc6906826cf126eda9ed7d382
IDM.air <innanadm8@gmail.com>
2019-06-16 14:00:24 -0400

    update

---
+++ routes/userRoutes.js
@@ -0,0 +1,33 @@
+// routes/users.js
+
+var express = require("express");
+var secured = require("../controllers/secured");
+var router = express.Router();
+var db = require("../models");
+
+/* GET user profile. */
+router.get("/user", secured(), function (req, res, next) {
+  const { _raw, _json, ...userProfile } = req.user;
+  console.log(req.user);
+  db.User.findOne({
+    
+  })
+  
+  //check to see if the user exists in the db. If not, route to a finish profile page, post all that crap in the db. 
+  // db.User.findOne({
+  //   where: {
+  //     id : req.body.params
+  //   }
+  // }).then(function (results) {
+    //...display the result data on the profile page, in the appropriate areas.
+    res.render("userProfile", {
+        userProfile: JSON.stringify(userProfile, null, 2),
+        title: "Profile page",
+        fullname: userProfile.displayName,
+        profileImg: userProfile.picture
+      });
+  });
+
+// });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,118 @@
-var app = require("./app");
-var db = require("./model");
+/* eslint-disable no-console */
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
+var express = require("express");
+var exphbs = require("express-handlebars");
+var bodyParser = require("body-parser")
+var apiRouter = require("./routes/apiRoutes.js");
+var authRouter = require("./routes/authRoutes.js");
+var userRouter = require("./routes/userRoutes.js")
+var userInViews = require("./controllers/userInViews");
+var session = require("express-session");
+var dotenv = require("dotenv");
+var passport = require("passport");
+var Auth0Strategy = require("passport-auth0");
+dotenv.config();
+var db = require("./models");
+var app = express();
+
+const crypto = require("crypto");
+const id = crypto.randomBytes(16).toString("hex");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// config express-session
+var sess = {
+  secret: id,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true
+};
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+console.log(app.get('env'));
+
+if (app.get('env') === 'production') {
+  sess.cookie.secure = true; // serve secure cookies, requires https
+}
+app.use(session(sess));
+
+
+// Configure Passport to use Auth0
+var strategy = new Auth0Strategy(
+  {
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL:
+      process.env.AUTH0_CALLBACK_URL || "http://localhost:8080/callback"
+  },
+  function (accessToken, refreshToken, extraParams, profile, done) {
+    // accessToken is the token to call Auth0 API (not needed in the most cases)
+    // extraParams.id_token has the JSON Web Token
+    // profile has all the information from the user
+    return done(null, profile);
+  }
+);
+
+passport.use(strategy);
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// You can use this section to keep a smaller payload
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
+
+
+// Sets up the Express App
+// =============================================================
 
 var PORT = process.env.PORT || 8080;
 
-db.sequelize.sync({ force: true }).then(() => {
-  app.listen(PORT, function() {
-    //eslint-disable-line
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  if (req.user) {
+    console.log(req.user);
+    res.render("home", { isLoggedIn: true, buttonData: "Your Profile" });
+  } else {
+    res.render("home", { isLoggedIn: false, buttonData: "Login/Signup" });
+  }
+});
+// Routes
+// =============================================================
+app.use("/", userRouter);
+app.use("/api", apiRouter);
+app.use("/", authRouter);
+app.use(userInViews());
+
+module.exports = app;
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function () {
+  app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
   });
 });
