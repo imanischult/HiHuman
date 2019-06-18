6d368ddda63fb3df8c04e7c7342a574aa2d4242b
IDM.air <innanadm8@gmail.com>
2019-06-17 22:53:22 -0400

    creating...

--- models/user.js
+++ models/user.js
@@ -1,16 +1,17 @@
 module.exports = function (sequelize, DataTypes) {
     var User = sequelize.define("User", {
-        firstName: {
+        authId: {
             type: DataTypes.STRING,
             allowNull: false,
+            unique: true
         },
-        lastName: {
+        name: {
             type: DataTypes.STRING,
-            allowNull: false
+            
         },
         userName: {
             type: DataTypes.STRING,
-            allowNull: false
+            
         },
         displayName: {
             type: DataTypes.STRING,
@@ -25,15 +26,15 @@ module.exports = function (sequelize, DataTypes) {
 
     });
 
-    // User.associate = function (models) {
-    //     // This will actually be User to Relationships``
-    //     // A Post can't be created without an Author due to the foreign key constraint
-    //     User.hasMany(models.Relationship, {
-    //         foreignKey: {
-    //             allowNull: false
-    //         }
-    //     });
-    // };
+    User.associate = function (models) {
+        // This will actually be User to Relationships``
+        // A Post can't be created without an Author due to the foreign key constraint
+        User.hasMany(models.Relationship, {
+            // foreignKey: {
+            //     allowNull: false
+            // }
+        });
+    };
 
     return User;
 };
